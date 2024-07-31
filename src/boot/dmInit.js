208156270fcad53003a3dfac7051d850d5c3556b
joestarhu <joestarhu@163.com>
2024-07-31 21:15:54 +0800

    dmInit update

--- src/boot/dmInit.js
+++ src/boot/dmInit.js
@@ -1,26 +1,28 @@
-import { boot } from 'quasar/wrappers'
+import { boot } from "quasar/wrappers"
 import { Dark } from "quasar"
 import { DMSETTINGS } from "src/base/dm"
 
-export default boot(({ app }) => {
-    // 主题色的导入
-    const mode = localStorage.getItem(DMSETTINGS.appearance) || "auto"
 
-    switch (mode) {
+export default boot(({ app }) => {
+    const appearance = localStorage.getItem(
+        DMSETTINGS.appearance
+    ) || "auto";
+
+    switch (appearance) {
         case "auto":
-            Dark.set(mode)
+            Dark.set(appearance);
             break;
         case "true":
-            Dark.set(true)
+            Dark.set(true);
             break;
         case "false":
-            Dark.set(false)
-            break
-        default:
+            Dark.set(false);
             break;
+        default:
+            // should not be here
+            Dark.set("auto");
+            break;
+
     }
+
 })
-
-
-
-
